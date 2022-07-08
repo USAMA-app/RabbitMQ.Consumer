f57a9afd0d9e57b53e6070e0962d45ca360a27cb
naveedekram <naveed.ekram@thesoftcube.com>
2022-07-09 04:34:49 +0500

    first commit

---
+++ RabbitMQ.Consumer/DirectExchangeConsumer.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    public static class DirectExchangeConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.ExchangeDeclare("demo-direct-exchange", ExchangeType.Direct);
+            channel.QueueDeclare("demo-direct-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            channel.QueueBind("demo-direct-queue", "demo-direct-exchange", "account.init");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, args) =>
+            {
+                var body = args.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+            Console.WriteLine("Consumer Started");
+            Console.ReadLine();
+
+        }
+    }
+}
